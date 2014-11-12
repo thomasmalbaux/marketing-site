83b7720f51d9fce53100aad3918f821b43f66229
Mikko Koski <mikkokos@gmail.com>
2014-11-12 21:36:13 +0200

    Existing account, enter, show

--- src/js/app/trial-form.js
+++ src/js/app/trial-form.js
@@ -7,33 +7,78 @@ define(['jquery', 'move', 'app/trial-form-validators'], function($, move, valida
     el = {
       emailSlide: $('#trial-email-slide'),
       emailInput: $('#trial-email-input'),
-      emailNextButton: $('#trial-email-next'),
+      emailForm: $('#trial-email-form'),
       emailNotSell: $('#trial-email-not-sell'),
-      emailInvalid: $('#trial-email-invalid')
+      emailInvalid: $('#trial-email-invalid'),
+
+      existingAccountSlide: $('#trial-existing-account')
     };
 
-    el['emailNextButton'].click(emailNextHandler);
+    el['emailForm'].submit(emailHandler);
   };
 
   var hide = function(slide) {
-    move(el.emailSlide[0])
+    move(slide[0])
       .translate(-300)
       .set('opacity', 0.1)
       .duration('0.2s')
       .then()
         .set('display', 'none')
+        .set('opacity', 1)
         .pop()
       .end();
   };
 
-  var emailNextHandler = function() {
+  var show = function(slide) {
+    move(slide[0])
+      .set('display', 'block')
+      .set('opacity', 0.1)
+      .x(300)
+      .duration(0)
+      .then()
+        .set('opacity', 1)
+        .x(-300)
+        .duration('0.2s')
+        .pop()
+      .end();
+  };
+
+  var checkEmailAvailability = function(email, success, fail) {
+    var request = $.ajax(
+      {
+        type: "GET"
+        , url: 'http://catch.sharetri.be/int_api/check_email_availability'
+        , data: {email: email}
+        , dataType: 'json'
+      });
+
+    request.done(function(response) {
+      success(response.available);
+    });
+    request.fail(fail);
+  };
+
+  var emailHandler = function(e) {
     var email = el['emailInput'].val();
     if (validator.validEmail(email)){
       hide(el.emailSlide);
+      checkEmailAvailability(email, function(available) {
+        if (available) {
+
+        } else {
+          show(el.existingAccountSlide);
+        }
+      }, function() {
+
+      });
     } else {
       el.emailNotSell.hide();
       el.emailInvalid.show();
     }
+
+    // Don't submit form
+    e.preventDefault();
+    return false;
   };
 
   return {
